4b746d628a6e0fa56d1048108e4c29387a9f5d01
Felipe Sugimoto <fe.sugi@gmail.com>
2021-05-19 20:08:24 +0900

    Update index.php

--- index.php
+++ index.php
@@ -13,22 +13,6 @@
 </head>
 <body>
 
-  <!-- Navigation-->
-  <nav class="navbar navbar-expand-lg navbar-light">
-      <div class="container">
-          <a class="navbar-brand" href="https://japao.maehara.com.br/">Japão</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-          <div class="collapse navbar-collapse" id="navbarResponsive">
-              <ul class="navbar-nav ml-auto">
-                  <li class="nav-item active"><a class="nav-link" href="https://japao.maehara.com.br/noticias/">Notícias</a></li>
-                  <li class="nav-item"><a class="nav-link" href="https://japao.maehara.com.br/pesquisas/">Pesquisas</a></li>
-                  <li class="nav-item"><a class="nav-link" href="https://japao.maehara.com.br/guias/">Guias</a></li>
-                  <li class="nav-item"><a class="nav-link" href="https://japao.maehara.com.br/sobre/">Sobre</a></li>
-              </ul>
-          </div>
-      </div>
-  </nav>
-
   <!-- Page Content-->
   <div class="container">
     <h2 align="center">Consulta de HS/NCM</h2>
